312a849583facb95142a0a7cf5899ded08b61a35
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-04-24 16:43:52 +0200

    archivo cuenta

---
+++ cuenta.py
@@ -0,0 +1,32 @@
+import threading
+
+class CuentaBancaria:
+    def __init__(self, cantidad):
+        self.cantidad = cantidad
+        self.lock = threading.Lock()
+    def ingresar(self, cantidad):
+        self.cantidad += cantidad
+    def retirar(self, cantidad):
+        self.cantidad -= cantidad
+    def get_cantidad(self):
+        return self.cantidad
+    
+class Ingresar(threading.Thread):
+    def __init__(self, cuenta, cantidad):
+        threading.Thread.__init__(self)
+        self.cuenta = cuenta
+        self.cantidad = cantidad
+    def run(self):
+        self.cuenta.lock.acquire()
+        self.cuenta.ingresar(self.cantidad)
+        self.cuenta.lock.release()
+
+class Retirar(threading.Thread):
+    def __init__(self, cuenta, cantidad):
+        threading.Thread.__init__(self)
+        self.cuenta = cuenta
+        self.cantidad = cantidad
+    def run(self):
+        self.cuenta.lock.acquire()
+        self.cuenta.retirar(self.cantidad)
+        self.cuenta.lock.release()
